cf61cb133c9d5e00041d34a022c739e73cf463f1
qlsun <qlsun@haiyi-info.com>
2020-04-09 16:00:20 +0800

    【CoreApi】支持EF连接MySql

---
+++ Development/CoreApi/CoreApi.Entity/MSSQLDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreApi.Entity
+{
+    /// <summary>
+    /// SQLServer数据库连接
+    /// </summary>
+    public class MSSQLDbContext : DbContext
+    {
+        public MSSQLDbContext(DbContextOptions<MSSQLDbContext> options) : base(options)
+        {
+
+        }
+
+        #region DbSet Entity映射到数据库表
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ Development/CoreApi/CoreApi.Entity/MYSQLDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApi.Entity
+{
+    /// <summary>
+    /// MySql数据库连接
+    /// </summary>
+    public class MYSQLDbContext : DbContext
+    {
+        public MYSQLDbContext(DbContextOptions<MYSQLDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
