c54f42c06e73d1dcddb1d18c70bb2e91a7893303
qlsun <qlsun@haiyi-info.com>
2020-04-26 14:48:48 +0800

    【CoreAPi】 新增Vue项目

---
+++ Development/CoreApi/CoreApi.Entity/Enums/ProtocolType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApi.Entity.Enums
+{
+    /// <summary>
+    /// 邮件相关协议
+    /// </summary>
+    public enum ProtocolType
+    {
+        /// <summary>
+        /// Simple Mail Transfer Protocal
+        /// </summary>
+        SMTP = 1,
+
+        /// <summary>
+        /// Post Office Protocal 3
+        /// </summary>
+        POP3 = 2,
+
+        /// <summary>
+        /// Internet消息访问协议
+        /// </summary>
+        IMAP = 3,
+    }
+}
