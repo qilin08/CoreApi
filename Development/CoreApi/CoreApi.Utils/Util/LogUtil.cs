a6ed15821d68f1a1f9de4bbc137f448e68a9e6cf
qlsun <qlsun@haiyi-info.com>
2020-07-23 17:51:59 +0800

    整合log4

---
+++ Development/CoreApi/CoreApi.Utils/Util/LogUtil.cs
@@ -0,0 +1,20 @@
+﻿/*********************************************************************************** 
+ *   Filename :LogUtil  
+ *   Date:2020/07/23 17:27:25 
+ *   Machinename:DESKTOP-28ROJ1R 
+ *   Created by: qlSun 
+ *   Description : 
+ * 
+ *   ChangeLog: 
+ *   2020/07/23 17:27:25: Created! 
+ ************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreApi.Utils.Util
+{
+    class LogUtil
+    {
+    }
+}
