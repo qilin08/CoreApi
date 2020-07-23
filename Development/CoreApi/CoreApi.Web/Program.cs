bb5ba9381efb6016c2dfa636e013c1a39a4076f2
qlsun <qlsun@haiyi-info.com>
2020-07-23 18:15:55 +0800

    符号

--- Development/CoreApi/CoreApi.Web/Program.cs
+++ Development/CoreApi/CoreApi.Web/Program.cs
@@ -43,21 +43,21 @@ namespace CoreApi.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                })
-                //.ConfigureLogging((context, loggingbuilder) =>
-                //{
-                //    ////该方法需要引入Microsoft.Extensions.Logging名称空间
-                //    //loggingbuilder.AddFilter("System", LogLevel.Warning); //过滤掉系统默认的一些日志
-                //    //loggingbuilder.AddFilter("Microsoft", LogLevel.Warning);//过滤掉系统默认的一些日志
-                //    ////添加Log4Net
-                //    ////var path = Directory.GetCurrentDirectory() + "\\log4net.config"; 
-                //    ////不带参数：表示log4net.config的配置文件就在应用程序根目录下，也可以指定配置文件的路径
-                //    //loggingbuilder.AddLog4Net();
+                });
+        //.ConfigureLogging((context, loggingbuilder) =>
+        //{
+        //    ////该方法需要引入Microsoft.Extensions.Logging名称空间
+        //    //loggingbuilder.AddFilter("System", LogLevel.Warning); //过滤掉系统默认的一些日志
+        //    //loggingbuilder.AddFilter("Microsoft", LogLevel.Warning);//过滤掉系统默认的一些日志
+        //    ////添加Log4Net
+        //    ////var path = Directory.GetCurrentDirectory() + "\\log4net.config"; 
+        //    ////不带参数：表示log4net.config的配置文件就在应用程序根目录下，也可以指定配置文件的路径
+        //    //loggingbuilder.AddLog4Net();
 
-                //    //var path = System.IO.Directory.GetCurrentDirectory();
-                //    //loggingbuilder.AddLog4Net($"{path}/log4net.config");//配置文件
+        //    //var path = System.IO.Directory.GetCurrentDirectory();
+        //    //loggingbuilder.AddLog4Net($"{path}/log4net.config");//配置文件
 
-                //    loggingbuilder.AddLog4Net();
-                //});
+        //    loggingbuilder.AddLog4Net();
+        //});
     }
 }
