aed9c7bc6ae3d188ad9f6f2c836523ef294bbfe4
qlSun <qlsun@haiyi-info.com>
2020-10-21 11:20:58 +0800

    验证Nginx配置

---
+++ Development/CoreApi/CoreApi.Web/Controllers/Nginx/NginxController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApi.Web.Controllers.Nginx
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NginxController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"{HttpContext.Connection.LocalIpAddress.ToString()}:{HttpContext.Connection.LocalPort.ToString()}";
+        }
+    }
+}
